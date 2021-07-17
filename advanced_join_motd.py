c3271fe18509d5b0570469c6b49bf24678c57cf1
ra1ny-yuki <wyb99@hotmail.com>
2021-07-17 09:41:49 +0800

    first test version

--- advanced_join_motd.py
+++ advanced_join_motd.py
@@ -2,6 +2,7 @@
 plg_name = 'Advanced Join MOTD'
 plg_id = 'advanced_join_motd'
 plg_desc = 'Custom your own join MOTD.'
+verbose_mode = False
 PLUGIN_METADATA = {
     'id': plg_id,
     'version': '4',
@@ -15,6 +16,7 @@ PLUGIN_METADATA = {
     }
 }
 
+from mcdreforged.api import rtext
 from mcdreforged.api.all import *
 from parse import parse
 from urllib.request import urlopen
@@ -27,6 +29,7 @@ import os
 import re
 import time
 import shutil
+import logging
 import datetime
 import collections
 
@@ -55,7 +58,7 @@ readme_text = '''
 可以利用转义字符为其添加各种元素
 1. # 仅适用于行首, 整行注释
 2. %day% 开服时间
-3. %serverlist: <servers>% 群组服子服列表, 引号后接子服务器列表<servers>, 使用空格分隔, 服务器列表不可换行
+3. %serverlist: <servers>% 群组服子服列表, 引号后接子服务器列表<servers>, 使用逗号分隔, 服务器列表不可换行
 4. %7<text>% 一段含点击事件的浅灰色字符, 点击事件为执行指令, 指令内容即为<text>代表的字符串
 5. %8<text>% 一段含点击事件的浅灰色字符, 点击事件为补全指令, 指令内容即为<text>代表的字符串
 6. %a<text>% 一段含点击事件的黄绿色字符, 点击事件为复制到剪贴板, 复制内容即为<text>代表的字符串
@@ -77,10 +80,110 @@ default_text = r'''
 
 old_text_file = 'config/adv_joinmotd.txt' # Old text file to auto migrate, do not change!
 style_elements = ['§4', '§c', '§6', '§e', '§2', '§a', '§b', '§3', '§1', '§9', '§d', '§5', '§f', '§7', '§8', '§0', '§r', '§l', '§o', '§n', '§m', '§k']
-click_events = {'7': RAction.run_command, '8': RAction.suggest_command, 'a': RAction.copy_to_clipboard, 'n': RAction.open_url}
 hover_element = {'7': '执行', '8': '补全', 'a': '复制', 'n': '访问'}
 time_format = collections.namedtuple('time_format', 'sec min hrs date mon yrs day')
+original_func = collections.namedtuple('Func', 'func exargs comment')
 
+
+class LineFormatter:
+    CLICK_EVENTS = {'7': RAction.run_command, '8': RAction.suggest_command, 'a': RAction.copy_to_clipboard, 'n': RAction.open_url}
+    pattern = r'%[\S ]*?%'
+
+    def __init__(self, text: str, player: str, server: ServerInterface):
+        self.raw_text = text
+        self.rtext_ori = re.findall(self.pattern, text)
+        self.normal_text = re.split(self.pattern, text)
+        self.player = player
+        self.server = server
+        return self
+
+    def __get_player(self, imports: str, *args, **kwargs) -> str:
+        return self.player
+
+    @staticmethod
+    def __get_daycount(imports: str, *args, **kwargs) -> str:
+        if daycount_imported:
+            return getday()
+        return f'%{imports}%'
+    
+    @staticmethod
+    def __get_day(imports: str, *args, **kwargs) -> str:
+        output = datetime.datetime.now() - datetime.datetime.strptime(imports[6:].strip(), '%Y-%m-%d')
+        return str(abs(output.days))
+
+    @staticmethod
+    def __get_serverlist(imports: str, *args, **kwargs) -> RTextList:
+        server_list = imports[11:].strip().split(',')
+        server_text = ''
+        for server_ in server_list:
+            l = parse('[{outer}]({inner})', server_.strip())
+            if l: outer, inner = l['outer'], l['inner']
+            else: outer, inner = server_.strip(), server_.strip()
+            server_text += rclick(f'[§7{outer}§r]', f'点击跳转到 §7{inner}§r', f'/server {inner}') + ' '
+        return server_text
+
+    def __get_playerlist(self, imports: str, *args, **kwargs) -> str:
+        raw_rcdata = self.server.rcon_query('list')
+        if raw_rcdata is not None:
+            player_list = str(raw_rcdata).split(' players online:')[1].strip().split(',')
+            playerlist = ''
+            for player_name in player_list:
+                if playerlist != '': playerlist += ', '
+                playerlist += player_name.strip()
+            return player_list
+        return f'%{imports}%'
+
+    @staticmethod
+    def __get_text_from_api(imports, *args, **kwargs) -> str:
+        try:
+            return urlopen(imports[4:]).read().decode('utf8').strip()
+        except:
+            return ''
+
+    @classmethod
+    def __get_click_event(cls, imports, *args, **kwargs) -> RTextBase:
+        l = parse('{event}[{name}]({content})', imports)
+        if l: pre, event, hover, name, content = l['event'], cls.CLICK_EVENTS[l['event']], hover_element[l['event']], l['name'], l['content']
+        else: pre, event, hover, name, content = list(imports)[0], cls.CLICK_EVENTS[list(imports)[0]], hover_element[list(imports)[0]], imports[1:], clean(imports[1:])
+        if pre == '8': pre, content = '7', content.strip() + ' '
+        return rclick(f"§{pre}{name}§r", f"点击{hover} §7{content}§r", content, event)
+
+
+    ACTIONS = {
+        'day': __get_day,
+        'player': __get_player,
+        'since:': __get_daycount,
+        'playerlist': __get_playerlist,
+        'serverlist:': __get_serverlist,
+        'API:': __get_text_from_api
+    }
+    cle = {}
+    for key in CLICK_EVENTS.keys(): cle[key] = __get_click_event
+    ACTIONS.update(cle)
+
+    @property
+    def formatted(self):
+        rtext_converted = []
+        for e in self.rtext_ori:
+            ele = e[1:-1].strip()
+            for key, value in self.ACTIONS.items():
+                if ele.startswith(key):
+                    rtext_converted.append(value(ele))
+
+        final_line = ''
+        for num in range(len(self.normal_text)):
+            final_line += str(self.normal_text[num])
+            if num + 1 == len(self.normal_text): break
+            final_line += rtext_converted[num]
+
+def convert_text(server: ServerInterface, text: str, player) -> RTextBase:
+    return_text = ''
+    for line in text.splitlines():
+        if return_text != '':
+            return_text += '\n'
+        if not line.startswith('#'):
+            return_text += LineFormatter(line, player, server).formatted
+    return return_text
 
 def get_text() -> str:
     text_file = os.path.join(config_folder, choose_file())
@@ -178,76 +281,11 @@ def converted(text, num, max):
     for item in text:
         text_after += item
     return text_after
-
-def convert_text(server: ServerInterface, text: str, player: str) -> RTextBase:
-    return_text = ''
-    for line in text.splitlines():
-        if not line.startswith('#'):
-            pattern = r'%[\S ]*?%'
-            rtext_ori = re.findall(pattern, line)
-            normal_text = re.split(pattern, line)            
-            rtext_converted = list()
-            for element in rtext_ori:
-                element_content = element[1:-1].strip()
-                if element_content == 'day' and daycount_imported:
-                    rtext_converted.append(getday())
-                elif element_content == 'player':
-                    rtext_converted.append(player)
-                elif element_content == 'playerlist':
-                    player_list = str(server.rcon_query('list')).split(' players online:')[1].strip().split(',')
-                    playerlist = ''
-                    for player_name in player_list:
-                        if playerlist != '': playerlist += ', '
-                        playerlist += player_name.strip()
-                    rtext_converted.append(playerlist)
-                elif element_content.startswith('serverlist:'): 
-                    server_list = element_content[11:].strip().split(',')
-                    server_text = ''
-                    for server_ in server_list:
-                        l = parse('[{outer}]({inner})', server_.strip())
-                        if l: outer, inner = l['outer'], l['inner']
-                        else: outer, inner = server_.strip(), server_.strip()
-                        server_text += rclick(f'[§7{outer}§r]', f'点击跳转到 §7{inner}§r', f'/server {inner}') + ' '
-                    rtext_converted.append(server_text)
-                elif list(element_content)[0] in click_events.keys():
-                    l = parse('{event}[{name}]({content})', element_content)
-                    if l: pre, event, hover, name, content = l['event'], click_events[l['event']], hover_element[l['event']], l['name'], l['content']
-                    else: pre, event, hover, name, content = list(element_content)[0], click_events[list(element_content)[0]], hover_element[list(element_content)[0]], element_content[1:], clean(element_content[1:])
-                    if pre == '8': pre, content = '7', content.strip() + ' '
-                    rtext_converted.append(rclick(f"§{pre}{name}§r", f"点击{hover} §7{content}§r", content, event))
-                elif element_content.startswith('API:'):
-                    rtext_converted.append(access_api(element_content[4:].strip()))
-                elif element_content.startswith('since:'):
-                    rtext_converted.append(get_day(element_content[6:].strip()))
-                else:
-                    rtext_converted.append(element)
-            num = 0
-            final_line = ''
-            while num <= len(normal_text):
-                final_line += str(normal_text[num])
-                num += 1
-                if num == len(normal_text):
-                    break
-                final_line += rtext_converted[num - 1]
-            if return_text != '':
-                return_text += '\n'
-            return_text += final_line
-    return return_text
-
-def get_day(startday: str):
-    output = datetime.datetime.now() - datetime.datetime.strptime(startday, '%Y-%m-%d')
-    return str(abs(output.days))
     
 def clean(msg: str):
     for item in style_elements:
         msg = msg.replace(item, '')
     return msg
-
-def access_api(url: str):
-    try:
-        return urlopen(url).read().decode('utf8').strip()
-    except:
-        return ''
 
 def on_player_joined(server: ServerInterface, player: str, info: Info):
     try:
@@ -256,6 +294,7 @@ def on_player_joined(server: ServerInterface, player: str, info: Info):
         server.tell(player, raw_text)
     except Exception as e:
         server.tell(player, f'Advanced Join MOTD出现错误, 请通知管理员处理, 错误如下: \n§c{e}§r')
+        logger.exception('AdvancedJoinMOTD出现异常, 错误如下: ')
         raise
 
 def rclick(message: str, hover_text: str, click_content: str, click_event = RAction.run_command, color = RColor.white, style = None):
@@ -272,7 +311,23 @@ def on_load(server: ServerInterface, prev_module):
     global logger
     generate_config(server)
     server.register_help_message(command, '显示欢迎消息')
-    logger = server.logger
+    
+    class DebugLogger(server.logger.__class__):
+        DEFAULT_NAME = 'AdvJoinMOTD'
+    
+        def debug(self, *args):
+            if verbose_mode:
+                super(server.logger.__class__, self).debug(*args)
+
+        def set_file(self, file_name):
+            if self.file_handler is not None:
+                self.removeHandler(self.file_handler)
+            self.file_handler = logging.FileHandler(file_name, encoding='UTF-8')
+            self.file_handler.setFormatter(self.FILE_FMT)
+            self.addHandler(self.file_handler)
+            return self
+
+    logger = DebugLogger(server)
 
 def generate_config(server: ServerInterface):
     def write_readme(readme_path):
